a92805ae7679de1b18cbb6f7060ec2b9ef87f0c0
Deepak <deepak2603dk@gmail.com>
2024-03-24 19:17:35 +0530

    undo redo functionality

--- src/app/_components/Board.tsx
+++ src/app/_components/Board.tsx
@@ -3,11 +3,14 @@ import { useEffect, useLayoutEffect, useRef } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { TOOL_ITEMS } from "./Constants";
 import {menuItemClick, activeItemClick} from '@/slice/menuSlice'
+import { countReset } from "console";
 
 const Board = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const shouldDraw = useRef<boolean>(false);
     const dispatch = useDispatch();
+    const drawHistory = useRef<Array>([]);
+    const drawPointer = useRef(0)
 
     const {activeMenuItem, actionMenuItem} = useSelector((state:any)=>state.menu)
         console.log('>>>>>>>>>>>ati', activeMenuItem)
@@ -30,11 +33,17 @@ const Board = () => {
         anchor.href = URL;
         anchor.download = `my_sketch${Math.random()*10}.jpeg`
         anchor.click()
-        dispatch(activeItemClick(null))
+ 
         // console.log('>>>>>>>>>>>', URL)
     }
+    else if(actionMenuItem === TOOL_ITEMS.UNDO || actionMenuItem === TOOL_ITEMS.REDO){
+      if(drawPointer.current > 0 && actionMenuItem === TOOL_ITEMS.UNDO )  drawPointer.current -= 1;
+      if(drawPointer.current < drawHistory.current.length -1 && actionMenuItem === TOOL_ITEMS.REDO )  drawPointer.current += 1;
+      const imageData = drawHistory.current[drawPointer.current]
+      context?.putImageData(imageData, 0 , 0)
+    }
+    dispatch(activeItemClick(null))
   },[actionMenuItem])
-  
 
     useEffect(() => {
         if (!canvasRef.current) return;
@@ -63,6 +72,12 @@ const Board = () => {
         canvas.height = window.innerHeight;
         canvas.width = window.innerWidth;
 
+        
+        if (context) {
+            context.fillStyle = "#ffffff"; // white color
+            context.fillRect(0, 0, canvas.width, canvas.height);
+        }
+
         const handleMouseDown = (e: MouseEvent) => {
             shouldDraw.current = true;
             if (context) {
@@ -79,6 +94,12 @@ const Board = () => {
 
         const handleMouseUp = () => {
             shouldDraw.current = false;
+            const imageData = context?.getImageData(0, 0, canvas.width, canvas.height);
+            drawHistory.current.push(imageData);
+            drawPointer.current = drawHistory.current.length - 1;
+
+
+
         };
 
         if (context) {
